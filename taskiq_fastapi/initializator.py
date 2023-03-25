9c976254649a5b242e3b646cc49c83e676403021
Pavel Kirilin <win10@list.ru>
2023-03-25 23:38:07 +0400

    Initial commit.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- taskiq_fastapi/initializator.py
+++ taskiq_fastapi/initializator.py
@@ -1,32 +1,71 @@
-from typing import Callable, Union
+from typing import Awaitable, Callable
+
 from fastapi import FastAPI, Request
 from taskiq import AsyncBroker, TaskiqEvents, TaskiqState
+from taskiq.cli.utils import import_object
 
 
-def startup_event_generator(app: FastAPI, with_dependencies: bool):
-    async def startup(state: TaskiqState):
+def startup_event_generator(app: FastAPI) -> Callable[[TaskiqState], Awaitable[None]]:
+    """
+    Generate shutdown event.
+
+    This function takes FastAPI application
+    and runs startup event on broker's startup.
+
+    :param app: fastapi application.
+    :returns: startup handler.
+    """
+
+    async def startup(state: TaskiqState) -> None:
+        state.fastapi_app = app
+        app.router.routes = []
         await app.router.startup()
 
     return startup
 
 
-def shutdown_event_generator(app: FastAPI):
-    async def startup(state: TaskiqState):
+def shutdown_event_generator(app: FastAPI) -> Callable[[TaskiqState], Awaitable[None]]:
+    """
+    Generate shutdown event.
+
+    This function takes FastAPI application
+    and runs shutdown event on broker's shutdown.
+
+    :param app: current application.
+    :return: startup event handler.
+    """
+
+    async def startup(_: TaskiqState) -> None:
         await app.router.shutdown()
 
     return startup
 
 
-def init(
-    broker: AsyncBroker,
-    app: Union[FastAPI, Callable[[], FastAPI]],
-    factory: bool = False,
-) -> None:
+def init(broker: AsyncBroker, app_path: str) -> None:
+    """
+    Add taskiq startup events.
+
+    This is the main function to integrate FastAPI
+    with taskiq.
+
+    This function imports fastapi application by
+    python's path string and adds startup events
+    for broker.
+
+    :param broker: current broker to use.
+    :param app_path: path to fastapi application.
+    :raises ValueError: if fastapi cannot be resolved.
+    """
     if not broker.is_worker_process:
         return
 
-    if factory:
+    app = import_object(app_path)
+
+    if not isinstance(app, FastAPI):
         app = app()
+
+    if not isinstance(app, FastAPI):
+        raise ValueError(f"'{app_path}' is not a FastAPI application.")
 
     broker.add_dependency_context({Request: Request({"app": app, "type": "http"})})
 
